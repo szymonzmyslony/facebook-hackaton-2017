ab410c5335a8c45cc8fd68cf784d86ab70147747
Szymon <szymon.zmyslony@gmail.com>
2017-03-11 14:50:24 +0000

    Changed apollo

--- js/reducers/store.js
+++ js/reducers/store.js
@@ -5,12 +5,19 @@
 import { combineReducers } from "redux";
 import navReducer from "NavigationReducer";
 import userReducer from "UserReducer";
-import { ApolloClient, createNetworkInterface } from "react-apollo";
+import {
+  ApolloClient,
+  createNetworkInterface,
+  addTypeName
+} from "react-apollo";
 import type { NavigationState } from "react-navigation";
 
 import type { State as UserState } from "UserReducer";
 export const client = new ApolloClient({
-  networkInterface: createNetworkInterface({ uri: "http://my-api.graphql.com" })
+  networkInterface: createNetworkInterface({
+    uri: "http://my-api.graphql.com"
+  }),
+  queryTransformer: addTypeName
 });
 
 const appReducer = combineReducers({
